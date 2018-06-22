193958e10589910fd27374438e8a678bd1bd0240
virchau13 <computernerd7777@gmail.com>
2018-06-23 03:15:39 +0800

    Removed jQuery from javascript

--- APP/script.js
+++ APP/script.js
@@ -1,7 +1,7 @@
-$(document).ready(() => {
-	let searchbar = $("#search")
-	searchbar.on("change",(e) => {
-		console.log(e.target.value)
+document.addEventListener("DOMContentLoaded", () => {
+	let searchbar = document.getElementById("search");
+	searchbar.addEventListener("keypress",(e) => {
+		console.log(e.target.value);
 		// GET data from server for rendering
 	})
 })
