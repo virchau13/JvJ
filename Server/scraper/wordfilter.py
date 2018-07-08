216fc08c6bf8f3f6d3ab85d26f7ca2267609cf6a
Jun <junyynyyn@users.noreply.github.com>
2018-07-08 18:19:19 +0800

    Optimization

--- Server/scraper/wordfilter.py
+++ Server/scraper/wordfilter.py
@@ -1,4 +1,5 @@
 import nltk
+import string
 from nltk.text import Text
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
@@ -8,31 +9,23 @@ import re
 from searchscraper import scrape_google
 
 tokenizer = RegexpTokenizer(r'\w+')
-
 lemmatizer = WordNetLemmatizer()
-
 stop_words = set(stopwords.words('english'))
-
 regex = re.compile('[^a-zA-Z]')
 
 def filter_words_from_search(search_results):
     site_list = []
-    punctuation = [",",".",":",";","'","-","!",'"',"-","|","_",""]
     if (len(search_results) > 0):
         for page_num in range(len(search_results)):
             filteredwords = []
             page = search_results[page_num]
-            tokens = tokenizer.tokenize(page['title'])
-            tokens += tokenizer.tokenize(page['description'])
-            tokens += tokenizer.tokenize(page['content'])
-            filteredtokens = [w for w in tokens]
-            filteredwords += [x for x in filteredtokens if (x not in punctuation and not x.isdigit())]
+            tokens = tokenizer.tokenize(page['title'] + " " + page['description'] + " " + page['content'])
+            filteredwords += [x for x in [w for w in tokens] if (x not in string.punctuation and not x.isdigit())]
             lemmatized_words = []
             for word in filteredwords:
-                filtered_word = regex.sub('', word).lower()
-                lemmatized = lemmatizer.lemmatize(filtered_word)
-                if lemmatized not in stop_words and lemmatized != "":
-                    lemmatized_words.append(lemmatized)
+                word = lemmatizer.lemmatize(regex.sub('', word).lower())
+                if word not in stop_words and word != "":
+                    lemmatized_words.append(word)
 
             site_list.append(Text(lemmatized_words))
             dict_list = []
@@ -43,11 +36,11 @@ def filter_words_from_search(search_results):
         return {'error': 500}
 
 def scraper(querystring):
-	results = scrape_google(querystring, 10, 'en')
+	results = scrape_google(querystring, 20, 'en')
 
 	return filter_words_from_search(results)
 
 # if __name__ == "__main__":
 #   print(scraper("sgcodecampus.com"))
 
-print(scraper("*"))
+print(scraper("sgcodecampus"))
