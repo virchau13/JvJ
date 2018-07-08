78e419cb3794e9188c5707e520cde4608661a065
virchau <computernerd7777@gmail.com>
2018-07-08 17:26:44 +0800

    hi

--- APP/script.js
+++ APP/script.js
@@ -1,9 +1,15 @@
+let ipaddr = /* Enter IP address here. e.g. */ '192.168.1.81';
+
 document.addEventListener("DOMContentLoaded", () => {
 	let searchbar = document.getElementById("search");
 	searchbar.addEventListener("keypress",(e) => {
 		if(e.which === 13){
 			// GET data from server for rendering
-			fetch("http://localhost:5000/scrape?querystring=" + e.target.value)
+			document.getElementById('cloud-container').innerHTML = `
+			<svg id="wordcloud" version="1.1" id= baseProfile="full" width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">
+			</svg>
+			`;
+			fetch("http://" + ipaddr + ":5000/scrape?querystring=" + e.target.value)
 			.then((data) => data.json())
 			.then((data) => graph(data))
 
@@ -15,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		let max = Math.max.apply(null, Object.values(fqdist));
 		for(let w in fqdist){
 			el = Object.assign(document.createElementNS("http://www.w3.org/2000/svg", "text"), {innerHTML: w});
-			let wantedPos = {x: Math.floor(Math.random() * 500), y: Math.floor(Math.random() * 500)};
+			let wantedPos = {x: Math.floor(Math.random() * 1000), y: Math.floor(Math.random() * 1000)};
 			el.setAttributeNS(null, 'fontsize', fqdist[w]/max * 40);
 			el.setAttributeNS(null, 'x', wantedPos.x);
 			el.setAttributeNS(null, 'y', wantedPos.y);
