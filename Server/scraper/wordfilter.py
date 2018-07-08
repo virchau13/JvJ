a2f5a814da8574f134394abc9df542eb0122aeb4
Jun <junyynyyn@users.noreply.github.com>
2018-07-08 17:29:32 +0800

    Fix * Error

--- Server/scraper/wordfilter.py
+++ Server/scraper/wordfilter.py
@@ -18,27 +18,29 @@ regex = re.compile('[^a-zA-Z]')
 def filter_words_from_search(search_results):
     site_list = []
     punctuation = [",",".",":",";","'","-","!",'"',"-","|","_",""]
+    if (len(search_results) > 0):
+        for page_num in range(len(search_results)):
+            filteredwords = []
+            page = search_results[page_num]
+            tokens = tokenizer.tokenize(page['title'])
+            tokens += tokenizer.tokenize(page['description'])
+            tokens += tokenizer.tokenize(page['content'])
+            filteredtokens = [w for w in tokens]
+            filteredwords += [x for x in filteredtokens if (x not in punctuation and not x.isdigit())]
+            lemmatized_words = []
+            for word in filteredwords:
+                filtered_word = regex.sub('', word).lower()
+                lemmatized = lemmatizer.lemmatize(filtered_word)
+                if lemmatized not in stop_words and lemmatized != "":
+                    lemmatized_words.append(lemmatized)
 
-    for page_num in range(len(search_results)):
-        filteredwords = []
-        page = search_results[page_num]
-        tokens = tokenizer.tokenize(page['title'])
-        tokens += tokenizer.tokenize(page['description'])
-        tokens += tokenizer.tokenize(page['content'])
-        filteredtokens = [w for w in tokens]
-        filteredwords += [x for x in filteredtokens if (x not in punctuation and not x.isdigit())]
-        lemmatized_words = []
-        for word in filteredwords:
-            filtered_word = regex.sub('', word).lower()
-            lemmatized = lemmatizer.lemmatize(filtered_word)
-            if lemmatized not in stop_words and lemmatized != "":
-                lemmatized_words.append(lemmatized)
-
-        site_list.append(Text(lemmatized_words))
-        dict_list = []
-        for site in site_list:
-            dict_list.append(dict(site.vocab()))
-    return dict_list
+            site_list.append(Text(lemmatized_words))
+            dict_list = []
+            for site in site_list:
+                dict_list.append(dict(site.vocab()))
+        return dict_list
+    else:
+        return {'error': 500}
 
 def scraper(querystring):
 	results = scrape_google(querystring, 10, 'en')
@@ -48,4 +50,4 @@ def scraper(querystring):
 # if __name__ == "__main__":
 #   print(scraper("sgcodecampus.com"))
 
-print(scraper("reddit"))
+print(scraper("*"))
