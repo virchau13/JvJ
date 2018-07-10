cb55483a5c9764a9b3ec222e5966ffee846c31d0
Jun <junyynyyn@users.noreply.github.com>
2018-07-10 16:34:34 +0800

    (Attempted) Optimization of word filter
    Remove Regex Recompile
    Removed FilteredWords and LemmatizeWords empty array stage
    LemmatizeWords is now a 1 line statement instead of 5

--- Server/scraper/wordfilter.py
+++ Server/scraper/wordfilter.py
@@ -8,37 +8,32 @@ import re
 
 from searchscraper import scrape_google
 
-tokenizer = RegexpTokenizer(r'\w+')
-lemmatizer = WordNetLemmatizer()
+tokenizer = RegexpTokenizer(r'[a-zA-Z]+')
+lemmatize = WordNetLemmatizer().lemmatize
 stop_words = set(stopwords.words('english'))
-regex = re.compile('[^a-zA-Z]')
 
 def filter_words_from_search(search_results):
-    site_list = []
     if (len(search_results) > 0):
+        site_list = []
         for page_num in range(len(search_results)):
-            filteredwords = []
             page = search_results[page_num]
             tokens = tokenizer.tokenize(page['title'] + " " + page['description'] + " " + page['content'])
-            filteredwords += [x for x in [w for w in tokens] if (x not in string.punctuation and not x.isdigit())]
-            lemmatized_words = []
-            for word in filteredwords:
-                word = lemmatizer.lemmatize(regex.sub('', word).lower())
-                if word not in stop_words and word != "":
-                    lemmatized_words.append(word)
-
+            filteredwords = [x for x in [w for w in tokens] if (x not in string.punctuation and not x.isdigit())]
+            lemmatized_words = [lemmatize(w.lower()) for w in filteredwords if (lemmatize(w.lower()) not in stop_words and w != "")]
             site_list.append(Text(lemmatized_words))
-            dict_list = []
-            for site in site_list:
-                dict_list.append(dict(site.vocab()))
+        dict_list = []
+        for site in site_list:
+            dict_list.append(dict(site.vocab()))
         return dict_list
     else:
         return {'error': 500}
 
 def scraper(querystring):
-	results = scrape_google(querystring, 20, 'en')
+	results = scrape_google(querystring, 10, 'en')
 
 	return filter_words_from_search(results)
 
 # if __name__ == "__main__":
 #   print(scraper("sgcodecampus.com"))
+
+# print(scraper("census"))
